a27576b99b733481fd2edacae2e12dcef106f0e7
Krutika <klatkar9@gmail.com>
2024-04-06 14:08:18 -0400

    adding declaration

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB700 – Assignment 06
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or 
+* distributed to other students.
+* 
+* Name: Krutika Latkar Student       ID: 153425228       Date: 6th Apr 2024
+*
+* Online (Cyclic) Link: ________________________________________________________
+*
+********************************************************************************/ 
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
